5089f609ae0db5de627017ef12f51f9db3f1daec
Agathe Lavigne <gotgini@gmail.com>
2023-10-11 17:08:27 +0200

    Nasa library page done

--- src/components/Nasa/Nasa.jsx
+++ src/components/Nasa/Nasa.jsx
@@ -18,32 +18,32 @@ import { TbApi } from "react-icons/tb";
 function Nasa() {
   return (
 	<div className='Nasa'>
+         <h2>NASA Library</h2>
      <Carousel>
         
                 <div>
-                    <img src={NasaImg2} />
+                    <img src={NasaImg2} alt="nasa library website"/>
                 </div>
                 <div>
-                    <img src={NasaImg1} />
+                    <img src={NasaImg1} alt="nasa library website"/>
                 </div>
                 <div>
-                    <img src={NasaImg3} />
+                    <img src={NasaImg3} alt="nasa library website"/>
                 </div>
                 <div>
-                    <img src={NasaImg4} />
+                    <img src={NasaImg4} alt="nasa library website"/>
                 </div>
                 <div>
-                    <img src={NasaImg5} />
+                    <img src={NasaImg5} alt="nasa library website"/>
                 </div>
                 <div>
-                    <img src={NasaImg6} />
+                    <img src={NasaImg6} alt="nasa library website"/>
                 </div>
                 <div>
-                    <img src={NasaImg7} />
+                    <img src={NasaImg7} alt="nasa library website"/>
                 </div>
       </Carousel>
       <div className='project-container'>
-        <h2>NASA Library</h2>
         <h4>About this project</h4>
         <p>NASA Library is a solo project. It is a full-stack MERN (MongoDB, Express, React, Node.js) CRUD web app. There are three different APIs from the NASA Website and my own API for the CRUD functionality. Every user can create a personal account by signing up and logging in. This website serves as a small NASA library, presenting the following features on the homepage: APOD (Astronomical Picture Of the Day), data on asteroids close to Earth, Mars images from Curiosity, supernova data and images, and an observations page for making personal notes.</p>
 
@@ -60,7 +60,7 @@ function Nasa() {
         </div>
 
         <h4>Challenges</h4>
-        <p>It was more difficult than I thought.I wasn't able to implement any of the IceBox features for my project. I was able to create my MVP, but as the week went by, I had to make the project a bit lighter to stay on track. The CRUD functionality was challenging, yet it was the best part of the project as I got to learn more about React. It also provided good practice with MongoDB, Mongoose, and Express.</p>
+        <p>It was more difficult than I thought. I wasn't able to implement any of the IceBox features for my project. I was able to create my MVP, but as the week went by, I had to make the project a bit lighter to stay on track. The CRUD functionality was challenging, yet it was the best part of the project as I got to learn more about React. It also provided good practice with MongoDB, Mongoose, and Express.</p>
 
 
       </div>
